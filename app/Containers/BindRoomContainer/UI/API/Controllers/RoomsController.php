7b6c1132b3f056709525fdf169ceea68019bd824
kkonshin <kkonshin@gmail.com>
2025-07-12 01:38:02 +0300

    Черновая структура контроллера комнат

---
+++ app/Containers/BindRoomContainer/UI/API/Controllers/RoomsController.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Containers\BindRoomContainer\UI\API\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class RoomsController extends Controller
+{
+    // TODO логировать запросы
+    public function __construct()
+    {
+    }
+
+    public function getAll()
+    {
+
+    }
+
+    public function getAvailable()
+    {
+
+    }
+
+    public function getBooked()
+    {
+
+    }
+
+    public function getRoomsEquipment()
+    {
+
+    }
+
+    public function getBookedForCurrentUser()
+    {
+
+    }
+
+    public function getBookedByUserId()
+    {
+
+    }
+
+    public function addRoomsToPool()
+    {
+
+    }
+
+    public function removeRoomsFromPool()
+    {
+
+    }
+}
