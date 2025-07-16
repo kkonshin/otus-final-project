3e721c3dcb1fc9651dba0db7f8f9deb224c41067
fav <a.fenin@soglasie.ru>
2025-07-16 19:57:06 +0300

    feature/bookings_crud Добавил фабрику для модели бронирования

--- app/Containers/BookingContainer/Models/Booking.php
+++ app/Containers/BookingContainer/Models/Booking.php
@@ -2,6 +2,8 @@
 
 namespace App\Containers\BookingContainer\Models;
 
+use App\Containers\BookingContainer\Factories\BookingFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,11 +21,17 @@ class Booking extends Model
     /**
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
+    protected function casts(): array {
         return [
             'start_at' => 'datetime',
             'end_at' => 'datetime',
         ];
+    }
+
+    /**
+     * @return BookingFactory|Factory
+     */
+    protected static function newFactory(): BookingFactory|Factory {
+        return BookingFactory::new();
     }
 }
