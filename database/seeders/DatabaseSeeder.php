d9025c4db537e2e61099f2cf579e4f6967aa3c42
fav <a.fenin@soglasie.ru>
2025-07-16 19:58:41 +0300

    feature/bookings_crud Добавил в фабрику бронирования в DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,15 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Containers\BookingContainer\Models\Booking;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-
     public function run(): void
     {
         $this->call([
             RoomSeeder::class,
         ]);
+
+        Booking::factory()->count(50)->create();
     }
 }
