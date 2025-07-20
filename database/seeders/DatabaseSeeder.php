bee55804a76f659f9f253d6ee51c583e3cf1949b
kkonshin <kkonshin@gmail.com>
2025-07-20 14:42:37 +0300

    Удален избыточный сидер комнат. Кол-во создаваемых фабриками элементов уменьшено для всех таблиц.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Containers\BookingContainer\Models\Booking;
+use App\Containers\RoomBookingContainer\Models\Room;
 use App\Containers\UserContainer\Models\User;
 use App\Containers\EquipmentContainer\Models\Equipment;
 use App\Containers\EquipmentContainer\Models\RoomEquipment;
@@ -12,15 +13,24 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            RoomSeeder::class,
-        ]);
+        Room::factory()
+            ->count(10)
+            ->create();
 
-        User::factory()->count(50)->create();
+        User::factory()
+            ->count(20)
+            ->create();
 
-        Booking::factory()->count(50)->create();
+        Booking::factory()
+            ->count(20)
+            ->create();
 
-        Equipment::factory()->count(50)->create();
-        RoomEquipment::factory()->count(50)->create();
+        Equipment::factory()
+            ->count(20)
+            ->create();
+
+        RoomEquipment::factory()
+            ->count(20)
+            ->create();
     }
 }
